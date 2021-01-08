a36b797332b02ba946178d93b2e800f1b4a9c5f2
Francisco Alberto <franapolinario@hotmail.com>
2021-01-08 15:15:40 -0300

    Add teste_.py where we test getScalarMetricWithTimeOfDayAnomalyParams

---
+++ teste_.py
@@ -0,0 +1,31 @@
+import nupic
+import json 
+import csv
+import pprint
+import matplotlib.pyplot as plt
+from datetime import datetime as dt
+from nupic.encoders.date import DateEncoder
+from nupic.encoders.random_distributed_scalar import RandomDistributedScalarEncoder
+from nupic.encoders.date import DateEncoder
+from nupic.algorithms.spatial_pooler import SpatialPooler
+from nupic.algorithms.temporal_memory import TemporalMemory
+from  nupic.algorithms.anomaly import Anomaly
+from nupic.algorithms.anomaly_likelihood import AnomalyLikelihood  
+from nupic.frameworks.opf.common_models.cluster_params import getScalarMetricWithTimeOfDayAnomalyParams 
+from nupic.frameworks.opf.model_factory import ModelFactory
+import numpy as np
+import pandas as pd
+import os
+import time
+
+t = getScalarMetricWithTimeOfDayAnomalyParams(  metricData = [0],
+                                                minVal = -150,
+                                                maxVal = 150,
+                                                tmImplementation = "cpp"    )
+
+
+
+pp = pprint.PrettyPrinter(indent = 4)
+pp.pprint(t)
+
+
