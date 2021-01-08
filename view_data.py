d14e30349ae159883fbb05721853a75536cd2e27
Francisco Alberto <franapolinario@hotmail.com>
2021-01-08 14:56:26 -0300

    Update view_data to show the anomaly and the sign in the same graf

--- view_data.py
+++ view_data.py
@@ -1,17 +1,32 @@
 import matplotlib.pyplot as plt
 import numpy as np
+
 import datetime 
 import os
-
+#a=7000000,b=7008000
 sign = np.load("./signs/sign.npy")
-anomaly_logscore = np.genfromtxt('anom_logscore_teste_1(class)_ver1_.txt', delimiter=',')
-#sign[7001500:7001600,1] = [2000 for i in sign[7001500:7001600,1]]
+anomaly_logscore = np.genfromtxt('anomaly_of_2_inputs/anom_logscore_teste_1(class)_ver1_.txt', delimiter=',')
+sign[7001500:7001520,1] = [3000 for i in sign[7001500:7001520,1]]
 
 
 #plt.plot(sign[:,0],sign[:,1])
 
-plt.plot(sign[7000000:7002000,0],sign[7000000:7002000,1])
 
+fig, ax1 = plt.subplots()
+color = 'tab:orange'
+ax1.set_xlabel('time(s)')
+ax1.set_ylabel('frequency(Hz)',color = color)
+ax1.plot(sign[7000000:7003000,0],sign[7000000:7003000,1],color = color)
+ax1.tick_params(axis='y', labelcolor = color)
+
+ax2 = ax1.twinx()
+
+color ='tab:blue'
+ax2.set_ylabel('anom_score')
+ax2.plot(sign[7000000:7003000,0], anomaly_logscore[:-1],color = color)
+ax2.tick_params(axis='y', labelcolor=color)
+
+fig.tight_layout()
 plt.show()
 
     
