Fixed the ERROR  where the loglikelihood was been calculated with the anomaly score not with  the probability likelihood, the latter is the correct argument

view_data.py
@@ -4,7 +4,7 @@ import datetime
 import os
 #a=7000000,b=7008000
 sign = np.load("./signs/sign.npy")
-anomaly_logscore = np.genfromtxt('anomalies/1_anom_probscore_.txt', delimiter=',')
+anomaly_logscore = np.genfromtxt('anomalies/1_anom_logscore_.txt', delimiter=',')
 
 #######################ERRO ARTIFICIAL ################################
 sign[7001500:7001550,1] = [250 for i in sign[7001500:7001550,1]]