cf9dcbfcf8aef29d58d7c03012deda5ee02f325d
Francisco Alberto <franapolinario@hotmail.com>
2021-01-20 14:49:41 -0300

    Update view_data to show all the anomaly outputs, including the signal plot

--- view_data.py
+++ view_data.py
@@ -2,30 +2,55 @@ import matplotlib.pyplot as plt
 import numpy as np
 import datetime 
 import os
-#a=7000000,b=7008000
+
+#########################CARREGANDO OS DADOS##############################
+
 sign = np.load("./signs/sign.npy")
-anomaly_logscore = np.genfromtxt('anomalies/1_anom_score_.txt', delimiter=',')
 
-#######################ERRO ARTIFICIAL ################################
+
+anomaly_score = np.genfromtxt('anomalies/1_anom_score_.txt', delimiter=',')
+anomaly_likelihood = np.genfromtxt('anomalies/1_anom_probscore_.txt', delimiter=',')
+anomaly_logscore = np.genfromtxt('anomalies/1_anom_logscore_.txt', delimiter=',')
+
+##########################################################################
+
+####################### INTERVALO ANALISADO ##############################
+
+a = 7000000
+b = 7003000
+
+##########################################################################
+
+#######################ERRO ARTIFICIAL ###################################
+
 sign[7001500:7001550,1] = [i+10 for i in sign[7001500:7001550,1]]
-#######################################################################
 
-#plt.plot(sign[:,0],sign[:,1])
+##########################################################################
 
+############################ PLOT ########################################
 
-fig, ax1 = plt.subplots()
+fig, axs = plt.subplots(4)
 color = 'tab:orange'
-ax1.set_xlabel('time(s)')
-ax1.set_ylabel('frequency(Hz)',color = color)
-ax1.plot(sign[7000000:7003000,0],sign[7000000:7003000,1],color = color)
-ax1.tick_params(axis='y', labelcolor = color)
-
-ax2 = ax1.twinx()
+axs[0].set_ylabel('frequency(Hz)')
+axs[0].set_title('data plot')
+axs[0].plot(sign[a:b,0],sign[a:b,1],color = color)
 
 color ='tab:blue'
-ax2.set_ylabel('anom_score')
-ax2.plot(sign[7000000:7003000,0], anomaly_logscore[:-1],color = color)
-ax2.tick_params(axis='y', labelcolor=color)
+axs[1].set_ylabel('anom_score')
+axs[1].set_title('anomaly score')
+axs[1].plot(sign[a:b,0], anomaly_score[:-1],color = color)
+
+color ='tab:red'
+axs[2].set_ylabel('anom_likelihood')
+axs[2].set_title('anomaly likelihood')
+axs[2].plot(sign[a:b,0], anomaly_likelihood[:-1],color = color)
+
+color ='tab:brown'
+axs[3].set_ylabel('anom_loglikelihood')
+axs[3].set_xlabel("time(ms)")
+axs[3].set_title('anomaly log likelihood')
+axs[3].plot(sign[a:b,0], anomaly_logscore[:-1],color = color)
+
 
 fig.tight_layout()
 plt.show()
